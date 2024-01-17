383c35a6ae65ce24485168ebab6503647020efac
prince-buha <150029430+prince-buha@users.noreply.github.com>
2024-01-17 15:05:52 +0530

    Update pr_1.5.cpp

--- pr_1.5.cpp
+++ pr_1.5.cpp
@@ -1,20 +1,22 @@
 /*
-5. WAP which illustrates the use of public and private access modifiers
-Enter  Value for N : 4
-4 This is Value of Public Data Member of Class home .
-*/
-
+	5. WAP which illustrates the use of public and private access modifiers.
+	Enter  member of your home N : 9
+9 This is Value of Public Data Member of class home .
+	*/
+	
 #include<iostream>
 using namespace std;
 class home{
-	int n;
 	public:
-		int n1;
+	int n;
+	
+		int member;
 };
 int main(){
 	home h;
-	cout<<"Enter  Value for N : ";
-	cin>>h.n1;
-	cout<<h.n1<<" This is Value of Public Data Member of Class home .";
+	cout<<"Enter  member of your home N : ";
+	cin>>h.member;
+	cout<<h.member<<" This is Value of Public Data Member of class home .";
+	
 	return 0;
 }
